97748285d6bfbb8f4e30bc18b9d66a073515b24e
userkim12 <potomq444@naver.com>
2023-01-06 17:29:30 +0900

    fix: Update for 오류수정

--- server/src/main/java/com/mainproject/domain/member/mapper/MemberMapper.java
+++ server/src/main/java/com/mainproject/domain/member/mapper/MemberMapper.java
@@ -2,7 +2,9 @@ package com.mainproject.domain.member.mapper;
 
 import com.mainproject.domain.member.dto.MemberDto;
 import com.mainproject.domain.member.entity.Member;
+import org.mapstruct.Mapper;
 
+@Mapper(componentModel = "spring")
 public interface MemberMapper {
     Member memberPostToMember(MemberDto.Post post);
 }
