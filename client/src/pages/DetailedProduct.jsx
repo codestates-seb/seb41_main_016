b04b6fe1955064403b7d1d64ec60bd91f55aab05
davina708 <davinida96@gmail.com>
2023-01-06 18:25:45 +0900

    [FE]fix: point컬러 수정

--- client/src/pages/DetailedProduct.jsx
+++ client/src/pages/DetailedProduct.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import styled from "styled-components";
+import LayoutContainer from "../components/LayoutContainer";
+
+const Box = styled.div`
+    height: 800px;
+`;
 
 export default function DetailedProduct() {
-  return <div>DetailedProduct</div>;
+    return (
+        <LayoutContainer>
+            <Box>DetailedProduct</Box>
+        </LayoutContainer>
+    );
 }
