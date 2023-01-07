fab9e680fdb13e84ac61f73b79b8863524aaadf3
davina708 <davinida96@gmail.com>
2023-01-07 18:04:53 +0900

    [FE]design: detail페이지 css구현(객실 사진,정보)

--- client/src/components/LayoutContainer.jsx
+++ client/src/components/LayoutContainer.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
-  width: 100%;
-  max-width: 1440px;
-  min-width: 1024px;
-  margin: 0 auto;
-  padding: 80px 24px 0 24px;
+    width: 100%;
+    max-width: 1264px;
+    min-width: 1024px;
+    margin: 0 auto;
+    padding: 60px 24px 0 24px;
 `;
 
 export default function LayoutContainer({ children }) {
-  return <Container>{children}</Container>;
+    return <Container>{children}</Container>;
 }
