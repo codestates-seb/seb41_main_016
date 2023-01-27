58a3988c9e2a49284ff0e34f9a64b2be4c6c3f45
Davina <111004706+davina708@users.noreply.github.com>
2023-01-28 00:37:44 +0900

    [FE]fix: 소셜로그인, 카카오결제 data 받아오기 로직  수정

--- client/src/pages/KakaoLogin.jsx
+++ client/src/pages/KakaoLogin.jsx
@@ -1,38 +1,35 @@
 import axios from "axios";
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import Loading from "../components/Loading";
 
 export default function KakaoLogin() {
-  const location = useLocation();
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const KAKAO_CODE = location.search.split("=")[1];
+    const location = useLocation();
+    const KAKAO_CODE = location.search.split("=")[1];
+    console.log(KAKAO_CODE);
 
-  const getKakaoToken = async () => {
-    try {
-      axios.get(`/auth/kakao/login?code=${KAKAO_CODE}`).then((res) => {
-        localStorage.clear();
-        localStorage.setItem("accessToken", res.data.accessToken);
-        localStorage.setItem("refreshToken", res.data.refreshToken);
-        localStorage.setItem("kakaoAccessToken", res.data.kakaoAccessToken);
-        localStorage.setItem("memberId", res.data.memberId);
-      });
-      setTimeout(() => {
-        window.close();
-      }, 500);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+    const getKakaoToken = async () => {
+        try {
+            axios.get(`/auth/kakao/login?code=${KAKAO_CODE}`).then((res) => {
+                localStorage.clear();
+                localStorage.setItem("accessToken", res.data.accessToken);
+                localStorage.setItem("refreshToken", res.data.refreshToken);
+                localStorage.setItem(
+                    "kakaoAccessToken",
+                    res.data.kakaoAccessToken
+                );
+                localStorage.setItem("memberId", res.data.memberId);
+            });
+            setTimeout(() => {
+                window.close();
+            }, 500);
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
-  useEffect(() => {
-    getKakaoToken();
-  }, []);
-  return (
-    <>
-      <Loading />
-    </>
-  );
+    useEffect(() => {
+        getKakaoToken();
+    }, []);
+    return <>{/* <Loading /> */}</>;
 }
